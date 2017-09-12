bbe674248f454c349097575c58d77175b53786ad
768840822 <768840822@qq.com>
2017-09-12 14:09:48 +0800

    Signed-off-by: 768840822 <768840822@qq.com>

---
+++ src/one/one.java
@@ -0,0 +1,28 @@
+package one;
+
+public class one {
+
+	
+	
+	//tcp和udp的区别
+	//hibernate
+	
+	
+	
+	public String change(String s) {
+		s = "222";
+		return s;
+	}
+
+	public static void main(String[] args) {
+		final StringBuffer a=new StringBuffer("immutable");
+		a.append("ssss");
+		System.out.println(a);
+		
+		
+		
+		
+		
+	}
+
+}
